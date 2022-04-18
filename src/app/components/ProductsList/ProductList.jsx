e9bec13e8679321b5660eb860c8494cb75a6dc0e
pwksn <pawelkopciara@interia.pl>
2022-04-18 11:05:50 +0200

    feat: Empty ProductsList view added

--- src/app/components/ProductsList/ProductList.jsx
+++ src/app/components/ProductsList/ProductList.jsx
@@ -1,23 +1,45 @@
 import React from 'react';
 import ProductTile from '../ProductTile/ProductTile';
+import { Icon, Text } from '@chakra-ui/react';
+import { IoClipboardOutline } from 'react-icons/io5';
 import './ProductList.css';
 
 const ProductsList = () => {
+	const productsCount = 8;
+
 	return (
 		<div className='products-list-container'>
-			<div className='products-tiles'>
-				<ProductTile />
-				<ProductTile />
-				<ProductTile />
-				<ProductTile />
-				<ProductTile />
-				<ProductTile />
-				<ProductTile />
-				<ProductTile />
-			</div>
-			<div className='products-pagination'>
-				<span>1 2 3</span>
-			</div>
+			{productsCount ? (
+				<>
+					<div className='products-tiles'>
+						<ProductTile />
+						<ProductTile />
+						<ProductTile />
+						<ProductTile />
+						<ProductTile />
+						<ProductTile />
+						<ProductTile />
+						<ProductTile />
+					</div>
+					<div className='products-pagination'>
+						<span>1 2 3</span>
+					</div>
+				</>
+			) : (
+				<div className='products-empty'>
+					<Icon
+						color='gray.400'
+						as={IoClipboardOutline}
+						className='products-empty-icon'
+					/>
+					<Text fontSize='xl' fontWeight='medium'>
+						Ooops... It's empty here
+					</Text>
+					<Text fontSize='sm' fontWeight='medium' color='gray.500'>
+						There are no products on the list
+					</Text>
+				</div>
+			)}
 		</div>
 	);
 };
